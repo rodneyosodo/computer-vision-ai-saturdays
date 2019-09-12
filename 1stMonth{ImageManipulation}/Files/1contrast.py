75fdcb4177f68e7581ccd441f8e1474de067b06b
Rodney Osodo <blackd0t@protonmail.com>
2019-09-12 22:45:28 +0300

    contrast example

--- 1stMonth{ImageManipulation}/Files/1contrast.py
+++ 1stMonth{ImageManipulation}/Files/1contrast.py
@@ -2,23 +2,23 @@ import numpy as np
 import cv2
 import math
 
-img = cv2.imread('../Images/bottle.jpg', cv2.IMREAD_GRAYSCALE)
-
+img = cv2.imread('../Images/bottle.jpg', cv2.IMREAD_ANYCOLOR)
 height = img.shape[0]
 width = img.shape[1]
 
-contrast = 2
+contrast = 5
 
 for i in np.arange(height):
     for j in np.arange(width):
-        a = img.item(i,j)
-        b = math.ceil(a * contrast)
-        if b > 255:
-            b = 255
-        img.itemset((i,j), b)
+        for k in range(0,3,1):
+            a = img.item(i,j,k)
+            b = math.ceil(a * contrast)
+            if b > 255:
+                b = 255
+            img.itemset((i,j,k), b)
 
 cv2.imwrite('../Images/1contrast.jpg', img)
 
-cv2.imshow('image',img)
+cv2.imshow('image2',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
