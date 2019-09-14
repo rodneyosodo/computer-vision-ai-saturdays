8819cb534263f548c113283693162b482d469268
Adam Rao <raoadam87@gmail.com>
2019-09-14 15:42:51 +0300

    4threshold my solution

--- 1stMonth{ImageManipulation}/Files/4threshold.py
+++ 1stMonth{ImageManipulation}/Files/4threshold.py
@@ -1,4 +1,22 @@
 import numpy as np
 import cv2
 
-threshold = 150
+threshold = 200
+
+
+img = cv2.imread('../Images/logan.jpg', cv2.IMREAD_GRAYSCALE)
+height = img.shape[0]
+width = img.shape[1]
+
+for i in np.arange(height):
+    for j in np.arange(width):
+            a = img.item(i, j)
+            
+            if a > threshold:
+                a = 255
+            img.itemset((i, j), a)
+
+cv2.imwrite('../Images/logan.jpg', img)
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
